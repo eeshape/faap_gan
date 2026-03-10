f2f70d8f99103ead86cb00cf91051c40cd521f5f
eeshape <eeshape@inu.ac.kr>
2026-03-10 15:57:18 +0000

    ~quality 08

--- train_faap_simclr_infonce_fix8gpu_anchor_quality_20260310.py
+++ train_faap_simclr_infonce_fix8gpu_anchor_quality_20260310.py
@@ -362,8 +362,8 @@ def parse_args() -> argparse.Namespace:
     # Training
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--epochs", type=int, default=24)
-    parser.add_argument("--batch_size", type=int, default=12)
-    parser.add_argument("--num_workers", type=int, default=6)
+    parser.add_argument("--batch_size", type=int, default=5)
+    parser.add_argument("--num_workers", type=int, default=8)
     parser.add_argument("--lr_g", type=float, default=5e-5)
     parser.add_argument("--lr_warmup_epochs", type=int, default=3)
     parser.add_argument("--seed", type=int, default=42)
